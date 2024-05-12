bad93d6d53e35b2464483f65b7ef728c2e4f684e
Saemi-H <sstle940104@gmail.com>
2024-05-13 08:34:30 +0900

    chore: merge main branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ImgUploadPage from "./pages/Img_ upload_page";
 import RootLayout from "./pages/Root";
+import SubmitInfoPage from './Pages/SubmitInfoPage';
+import InfoDisplayPage from './Pages/InfoDisplayPage';
 
 const router = createBrowserRouter([
   {
@@ -12,6 +14,8 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/upload_image", element: <ImgUploadPage /> },
+      { path: "/submit", element: <SubmitInfoPage /> },
+      { path: "/result", element: <InfoDisplayPage /> },
     ],
   },
 ]);
