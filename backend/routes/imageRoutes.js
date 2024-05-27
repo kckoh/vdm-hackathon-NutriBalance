6adab6ff0cd2d519ea5271f764db723f6663b13e
Seungyeon Ji <jiseungyeon0321@gmail.com>
2024-05-27 07:23:31 -0700

    feat: Added the lists of top20 nutrients can be taken from suppliments

--- backend/routes/imageRoutes.js
+++ backend/routes/imageRoutes.js
@@ -2,22 +2,36 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const path = require("path");
+const nutrientsList = require("../lib/nutrientsLists");
+const Fuse = require("fuse.js");
 
 const { createWorker } = require("tesseract.js");
+const { getNutrientsNames } = require("../util/index");
 
 //I need to get image from front here, and need to transform in
 //over
 
 //이미지 찍을때 control을 할 수 없을까?
 
-//
+//Get Top 20 nutrients that can be taken from supplements for adult men and women
+const nutrientsNameLists = async () => {
+  const names = await getNutrientsNames();
+  console.log(names);
+};
 
-//1. 영양제로 섭취간으한 성인이 필요로하는 주요 영양소 Top10
-//2. Top10 - 해당 영양소를 이용해서 이미지에 포함되는
+nutrientsNameLists();
 
 //error hanling - 1. there is no text in the image
 //error handling - 2. there is no text related to our topic
 
+// Compare the text in the image with the top 20 nutriens
+const options = {
+  includeScore: true,
+  threshold: 0.3,
+  keys: ["name"],
+};
+
+// Image handling
 const upload = multer({
   storage: multer.diskStorage({
     destination(req, file, done) {
@@ -31,10 +45,6 @@ const upload = multer({
   }),
   limits: { fieldNameSize: 20 * 1024 * 1024 },
 });
-
-// const worker = createWorker({
-//   logger: (m) => console.log(m), // 로그 출력 설정
-// });
 
 router.post("/send", upload.single("file"), async (req, res) => {
   try {
