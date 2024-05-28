55624c159d508d848fed393052ba091844547e5e
Saemi-H <sstle940104@gmail.com>
2024-05-28 09:44:49 +0900

    feat: add Link to home if clicking logo icon

--- frontend/src/components/NavBar/index.js
+++ frontend/src/components/NavBar/index.js
@@ -1,12 +1,17 @@
+import { Link } from "react-router-dom";
+
+
 const NavPage = () => {
   return (
     <>
       <div className="p-4 bg-[#81C667]">
         <div className="m-auto flex" style={{justifyContent: 'space-between'}}>
           <h1 >
+            <Link to="/">
             <button>
               <img src='Icons/logoIcon.png' alt='logo'/>
             </button>
+            </Link>
           </h1>
           <div className="text-3xl font-bold py-0.5 text-white pr-">
             <button>NutriBalance</button>
